95d794c041e5ef4cc1bd4eec0283c5a96128e613
knikolaeff <kir1llnikolaeff@yandex.by>
2022-04-04 23:22:13 +0100

    Added a functionality to change files format without choosing any action. Added messageboxes

--- compressio.py
+++ compressio.py
@@ -14,12 +14,10 @@ class Main(qtw.QWidget):
 
         self.setWindowTitle("Compressio")
 
+        # These attributes are made to avoid passing directories from method to method
         self.newImgFormat = None
         self.sourceDir = None
         self.destDir = None
-
-        self.okMsg = qtw.QMessageBox()
-        self.okMsg.setText("Done!")
 
         self.notOkMsg = qtw.QMessageBox()
         self.notOkMsg.setText("Something went wrong")
@@ -29,59 +27,67 @@ class Main(qtw.QWidget):
 
         self.ui.proceedAllBtn.clicked.connect(self.proceedAll)
 
+    def showDoneMessage(self):
+        qtw.QMessageBox.information(self, "Success", "Done! Successfully edited images: %d")
+
     def openSourceDirectory(self):
-        sourceDirectory = qtw.QFileDialog.getExistingDirectory(
+        source_directory = qtw.QFileDialog.getExistingDirectory(
             self, "Choose a file", "")
 
-        if sourceDirectory:
-            self.ui.sourceEntry.setText("{}".format(sourceDirectory))
+        if source_directory:
+            self.ui.sourceEntry.setText("{}".format(source_directory))
 
     def openDestDirectory(self):
-        destDirectory = qtw.QFileDialog.getExistingDirectory(
+        dest_directory = qtw.QFileDialog.getExistingDirectory(
             self, "Choose a file", "")
 
-        if destDirectory:
-            self.ui.destinationEntry.setText("{}".format(destDirectory))
+        if dest_directory:
+            self.ui.destinationEntry.setText("{}".format(dest_directory))
 
-    def compress(self):
+    def getImgFormat(self):
+        self.newImgFormat = self.ui.formatBox.currentText()
+
+        if self.newImgFormat == "Original":
+            self.newImgFormat = None
+
+    # Overwrites sourceDir and destDir attributes with the ones in input fields
+    def getSources(self):
+        self.sourceDir = r"" + self.ui.sourceEntry.text() + "/"
+
+        if self.ui.overwriteCheck.isChecked() is not True:
+            self.destDir = r"" + self.ui.destinationEntry.text() + "/"
+        else:
+            self.destDir = self.sourceDir
+
+    def compressAndSave(self, image, fext, new_file_path):
+        quality = self.ui.qualitySpinbox.value()
+        # PNG is a lossless format, hence requires separate algorithm
+        if fext == ".png":
+            image = image.convert(
+                'P',
+                palette=Image.ADAPTIVE,
+                colors=256
+            )
+        image.save(new_file_path, self.newImgFormat, optimize=True, quality=quality)
+
+    def proceedAll(self):
         self.getSources()
-        self.saveAs()
+        self.getImgFormat()
         for file in os.listdir(self.sourceDir):
             fname, fext = os.path.splitext(file)
             # Saves image in a format, chosen in a spinbox, if the value in the spinbox != "Original"
             if self.newImgFormat is not None:
-                fext = self.newImgFormat.lower()
-            newFilePath = self.destDir + fname + "_compressio" + fext
+                fext = '.' + self.newImgFormat.lower()
+            new_file_path = self.destDir + fname + "_compressio" + fext
             image = Image.open(self.sourceDir + file)
-            quality = self.ui.qualitySpinbox.value()
-            if fext == ".png":
-                image = image.convert(
-                    'P',
-                    palette=Image.ADAPTIVE,
-                    colors=256
-                )
-            image.save(newFilePath, self.newImgFormat, optimize=True, quality=quality)
-        self.okMsg.exec()
-
-    def saveAs(self):
-        self.newImgFormat = self.ui.formatBox.currentText()
-        if self.newImgFormat == "Original":
-            self.newImgFormat = None
-
-    def getSources(self):
-        sourceDir = r"" + self.ui.sourceEntry.text() + "/"
-        if self.ui.overwriteCheck.isChecked() is not True:
-            destDir = r"" + self.ui.destinationEntry.text() + "/"
-        else:
-            destDir = sourceDir
-        return sourceDir, destDir
-
-    def proceedAll(self):
-
-        if self.ui.compressCheck.isChecked():
-            self.compress()
-        elif self.ui.resizeCheck.isChecked():
-            pass
+            if self.ui.resizeCheck.isChecked():
+                pass
+            if self.ui.compressCheck.isChecked():
+                self.compressAndSave(image, fext, new_file_path)
+            else:
+                # If no checkboxes ticked, saves images in a chosen format
+                image.save(new_file_path, self.newImgFormat)
+        self.showDoneMessage()
 
 
 if __name__ == '__main__':
